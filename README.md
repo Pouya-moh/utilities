5557c2612340b6b9960abadb0376ebc0ef6dbe15
Pouya-moh <other.pouya@gmail.com>
2016-06-28 10:46:48 +0200

    add csv-to-std-vector

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # utilities
-Tiny tools to help develpment
+Tiny tools to help development. Each directory in this repo contains a single tool (or a collection of conceptually related tools).
+
+## TOC
+1. [csv-to-std-vector]() is a header only tool to parse and convert csv file to ~std::vector<std::vector<double> >~.
---
+++ csv-to-std-vector/csv-to-std-vector.h
@@ -0,0 +1,35 @@
+#ifndef CSVTOSTDVECTOR_H
+#define CSVTOSTDVECTOR_H
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <stdlib.h>
+
+/**
+    This is a simple method that converts a csv file to a vector of vector fo doubles
+    It works byt it is not the best way, nor is the most elegant or efficient way.
+*/
+
+inline std::vector<std::vector<double> > parseCSVtoSTD(std::string path_to_file, char delimiter){
+    std::vector<std::vector<double> > ret;
+    std::ifstream input_file(path_to_file.c_str());
+    std::string line;
+
+    while (getline(input_file, line)) {
+        std::stringstream linestream(line);
+        std::string value;
+        std::vector<double> single_row;
+        int idx = 0;
+        while (getline(linestream, value, delimiter)){
+            double tmp = ::atof(value.c_str());
+            single_row.push_back(tmp);
+            idx++;
+        }
+        ret.push_back(single_row);
+    }
+    return ret;
+}
+
+#endif // CSVTOSTDVECTOR_H
