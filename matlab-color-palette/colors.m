c6fcd095d8d367c217b31fd231f1d2c6adba81a1
Pouya-moh <other.pouya@gmail.com>
2016-07-11 17:17:21 +0200

    add matlab color palette

---
+++ matlab-color-palette/colors.m
@@ -0,0 +1,21 @@
+%% Matlab's color palette since R2014b
+%  I put it here just as reference. Mode in matlab documentation
+%  at http://www.mathworks.com/help/matlab/graphics_transition/why-are-plot-lines-different-colors.html
+
+c1 = [0.0000 	0.4470    0.7410]; % blue
+c2 = [0.8500    0.3250    0.0980]; % red
+c3 = [0.9290    0.6940    0.1250]; % orange
+c4 = [0.4940    0.1840    0.5560]; % violet
+c5 = [0.4660    0.6740    0.1880]; % green
+c6 = [0.3010    0.7450    0.9330]; % cyan
+c7 = [0.6350    0.0780    0.1840]; % brown
+
+%% old palette:
+%  use the new one. much better!
+% c1 = [0.0000    0.0000    1.0000]; % blue
+% c2 = [0.0000    0.5000    0.0000]; % green
+% c3 = [1.0000    0.0000    0.0000]; % red
+% c4 = [0.0000    0.7500    0.7500]; % cyan
+% c5 = [0.7500    0.0000    0.7500]; % magenta
+% c6 = [0.7500    0.7500    0.0000]; % dark yellow
+% c7 = [0.2500    0.2500    0.2500]; % grey-ish-yellow puke
