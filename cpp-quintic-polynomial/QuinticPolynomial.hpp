2af2408dfb48b2ec00e8010ba5db8e5058f0ef82
pouya-moh <other.pouya@gmail.com>
2016-11-30 17:43:37 +0100

    add dof size, need to test if vector.size() is a thing

--- cpp-quintic-polynomial/QuinticPolynomial.hpp
+++ cpp-quintic-polynomial/QuinticPolynomial.hpp
@@ -1,3 +1,12 @@
+/*
+ * File:   QuinticPolynomial.hpp
+ * Author: Pouya Mohammadi
+ *
+ * Created on Friday June 3, 2015, 11:57
+ * This is based on quintic ploynomial presented in 
+ * Prof. Allesandro de Luca's slides of robotics 1.
+ */
+
 #ifndef QUINTICPOLYNOMIAL_HPP
 #define QUINTICPOLYNOMIAL_HPP
 
@@ -11,12 +20,15 @@ public:
     double end_time;
     double deltaT;
 
+    int dof;
+
     Eigen::VectorXd Qi;
     Eigen::VectorXd Qf;
     Eigen::VectorXd deltaQ;
 
     QuinticPolynomial();
     QuinticPolynomial(double _start_time, double _end_time, Eigen::VectorXd _Qi, Eigen::VectorXd _Qf);
+    void setParams(double _start_time, double _end_time, Eigen::VectorXd _Qi, Eigen::VectorXd _Qf);
     Eigen::VectorXd getQ(double time);
     Eigen::VectorXd getQd(double time);
     Eigen::VectorXd getQdd(double time);
